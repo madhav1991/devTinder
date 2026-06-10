f4d379df4034a1bc2808b9f2c516255e5da38022
Madhav Peri <prn.madhav@gmail.com>
2026-06-09 23:08:37 -0500

    feat: Make get, post api calls

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ const express = require("express");
 const app = express();
 
 
+// will only handle GET call to user
+app.get("/user", (req, res) => {
+    res.send({
+        name: "Madhav",
+        age: 23,
+    })
+})
+
+app.post("/user", (req, res) => {
+    console.log("Post request to DB");
+    res.send("Data saved successfully!");
+})
+
+// app.use will match all HTTP requests to /test
+app.use("/test", ((req, res) => {
+    res.send("Test Route Hit");
+}))
+
+
 app.listen(3001, () => {
     console.log(`Server is running on port 3000`);
 });
 
-app.use((req, res) => {
-    res.send("This is dev tinder!");
-})
+// app.use((req, res) => {
+//     res.send("This is dev tinder!");
+// })
