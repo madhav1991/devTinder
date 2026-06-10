bdadeaaad22d590b9ab0a57e4a82e9d8cb659b2d
Madhav Peri <prn.madhav@gmail.com>
2026-06-09 23:30:00 -0500

    Try url params

--- src/app.js
+++ src/app.js
@@ -3,22 +3,22 @@ const app = express();
 
 
 // will only handle GET call to user
-app.get("/user", (req, res) => {
-    res.send({
-        name: "Madhav",
-        age: 23,
-    })
-})
+// app.get("/user", (req, res) => {
+//     res.send({
+//         name: "Madhav",
+//         age: 23,
+//     })
+// })
 
-app.post("/user", (req, res) => {
-    console.log("Post request to DB");
-    res.send("Data saved successfully!");
-})
+// app.post("/user", (req, res) => {
+//     console.log("Post request to DB");
+//     res.send("Data saved successfully!");
+// })
 
-// app.use will match all HTTP requests to /test
-app.use("/test", ((req, res) => {
-    res.send("Test Route Hit");
-}))
+// // app.use will match all HTTP requests to /test
+// app.use("/test", ((req, res) => {
+//     res.send("Test Route Hit");
+// }))
 
 
 app.listen(3001, () => {
