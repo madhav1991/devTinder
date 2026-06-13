a3d69c35aaab83936c1b50828c71ce05c652407b
Madhav Peri <prn.madhav@gmail.com>
2026-06-12 19:02:16 -0500

    create user mongoose model

---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.model({
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    age: {
+        type: Number,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    }
+}, { timestamps: true });
+
+module.exports = userSchema;
