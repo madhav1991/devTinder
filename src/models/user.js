dcb8903bde44f684b61bd52f548c5aadd08a85af
Madhav Peri <prn.madhav@gmail.com>
2026-06-14 00:35:18 -0500

    Add email validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,7 +17,12 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("not valid email" + value)
+            }
+        }
     },
     password: {
         type: String,
